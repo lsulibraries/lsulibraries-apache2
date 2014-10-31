347ff8c8e006e5b2ac6bc42853e78b5c2936b762
Jacob Sanford <jsanford@unb.ca>
2014-10-31 07:56:20 -0300

    Update html dir for new apache

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,6 +1,6 @@
 override['set_fqdn'] = node['razor_metadata']['hostname']
 default['apache']['default_site_enabled'] = true
-default['apache']['docroot_dir'] = '/var/www'
+default['apache']['docroot_dir'] = '/var/www/html'
 
 default['unblibraries-firewall']['additional-rules']['apache_standard'] = '-A INPUT -p tcp -m tcp --dport 80 -j ACCEPT'
 default['unblibraries-firewall']['additional-rules']['apache_ssl'] = '-A INPUT -p tcp -m tcp --dport 443 -j ACCEPT'
